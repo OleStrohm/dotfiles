cdf7e63d860014cc91fed276f4df82349c0e5f8d
Ole Strohm <strohm99@gmail.com>
2021-02-03 04:05:18 +0000

    Fixed focusing issue

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -434,7 +434,7 @@ awful.rules.rules = {
     { rule = { },
       properties = { border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
-                     -- focus = awful.client.focus.filter,
+                     focus = awful.client.focus.filter,
                      raise = true,
                      keys = clientkeys,
                      buttons = clientbuttons,
